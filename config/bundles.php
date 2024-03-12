6d862921f80fa1852ad9e3b358f32953889462e7
Remy Jelu <remy.jelu.freelance@gmail.com>
2024-03-12 15:59:15 +0100

    Api Platform CRUD

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
--- src/Entity/TodoList.php
+++ src/Entity/TodoList.php
@@ -2,10 +2,39 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\TodoListRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: TodoListRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/todolist/{id}',
+        ),
+        new GetCollection(
+            uriTemplate: '/todolist/list',
+        ),
+        new Put(
+            uriTemplate: '/todolist/put/{id}',
+        ),
+        new Post(
+            uriTemplate: '/todolist/post',
+        ),
+        new Patch(
+            uriTemplate: '/todolist/patch/{id}',
+        ),
+        new Delete(
+            uriTemplate: '/todolist/delete/{id}',
+        )
+    ]
+)]
 class TodoList
 {
     #[ORM\Id]
